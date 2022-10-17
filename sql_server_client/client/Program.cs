efab0686dfcac1cee280b5f6c4df1c25f625d3b1
Manoj <manojparasuram.sadhanala@gmail.com>
2022-10-17 22:32:07 +0530

    added files

---
+++ sql_server_client/client/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+using System.Reflection;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Numerics;
+
+namespace appgui;
+
+
+public class Form1 : Form
+{
+    public Button button1;
+    public Button button2;
+    public TextBox textInputTextBox;
+    public Button textInputeButton;
+    //public bool license_valid;
+    //public MainMenu Menu;
+    public Form1()
+    {
+        Label ip_label = new Label();
+        ip_label.Text = "License Key";
+        ip_label.Location = new Point(25, 30);
+        ip_label.AutoSize = true;
+        ip_label.Font = new Font("Calibri", 10);
+        ip_label.Padding = new Padding(6);
+        this.Controls.Add(ip_label);
+
+        textInputTextBox = new TextBox();
+        textInputTextBox.Location = new Point(110, 30);
+        textInputTextBox.Size = new Size(120, 20);
+        this.Controls.Add(textInputTextBox);
+
+        Size = new Size(300, 150);
+        button2 = new Button();
+        button2.Size = new Size(60, 25);
+        button2.Location = new Point(120, 60);
+        button2.Text = "Run";
+        this.Controls.Add(button2);
+        button2.Click += new EventHandler(license_click);
+    }
+
+    private void license_click(object sender, EventArgs e)
+    {
+
+        if (String.IsNullOrEmpty(textInputTextBox.Text))
+        {
+            MessageBox.Show("Please enter a valid license key");
+            return;
+        }
+        else if (Convert.ToBase64String(Encoding.ASCII.GetBytes(textInputTextBox.Text)) == "MTIzNDcyMzA5NTcyMzkwNTM=")
+        {
+            var ip = "127.0.0.1";
+            byte[] msg = Encoding.ASCII.GetBytes("1");
+            //MessageBox.Show("1");
+            IPAddress address = IPAddress.Parse(ip);
+            //MessageBox.Show(ip);
+            //MessageBox.Show(address.ToString());
+            //MessageBox.Show("2");
+            IPEndPoint endPoint = new IPEndPoint(address, 8080);
+            //MessageBox.Show(endPoint.ToString());
+            //MessageBox.Show("3");
+            Socket Sock = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            //MessageBox.Show("4");
+            Sock.Connect(endPoint);
+            //MessageBox.Show(endPoint.ToString());
+            //MessageBox.Show("5");
+            Sock.Send(msg, msg.Length, 0);
+            byte[] buffer = new byte[1024];
+            int recieved = Sock.Receive(buffer);
+            byte[] data = new byte[recieved];
+            Array.Copy(buffer, data, recieved);
+            MessageBox.Show(Encoding.ASCII.GetString(data));
+            //MessageBox.Show("6");
+            Sock.Close();
+            return;
+        }
+        else
+        {
+            MessageBox.Show("Please enter a valid license key");
+            return;
+        }
+    }
+}
+
+static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }
+}
---
+++ sql_server_client/server/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+
+        // In Windows we are going to bind to the loopback address
+        // In Docker we have to bind to the actual IPAddress in order to make it accessible from outside
+        // The decision to bind to ip or loopback is done based on an environment variable bind_ip
+
+        IPAddress hostIP = IPAddress.Loopback;
+        //Console.WriteLine(hostIP);
+
+        if (hostIP == null) throw new ArgumentException("No valid IPv4 IPAddress to bind to");
+
+        IPEndPoint ep = new IPEndPoint(hostIP, 8080);
+        //Console.WriteLine(ep);
+        listenSocket.Bind(ep);
+        listenSocket.Listen(1);
+        //Console.WriteLine("1");
+
+        while (true)
+        {
+            Socket clientSocket = listenSocket.Accept();
+            byte[] buffer = new byte[1024];
+            //Console.WriteLine("2");
+            int received = clientSocket.Receive(buffer);
+            byte[] data = new byte[received];
+            //Console.WriteLine("3");
+            Array.Copy(buffer, data, received);
+            string text = Encoding.ASCII.GetString(data);
+            Console.WriteLine("Received: {0}", text);
+            if (text == "1")
+            {
+                byte[] msg = Encoding.ASCII.GetBytes("cyberchaze{q1w&2e3#r4t5A^y6#u7i%8o9p0}");
+                clientSocket.Send(msg, msg.Length, 0);
+            }
+            //byte[] response = Encoding.ASCII.GetBytes("cyberchaze{35#xmYii&PY5#ch#gH^6ey}");
+            //Console.WriteLine(Encoding.ASCII.GetString(response));
+            //clientSocket.Send(response);
+            clientSocket.Close();
+        }
+    }
+}
