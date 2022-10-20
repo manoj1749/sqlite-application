1db831528e7d7ba8bb807436363a6c384774e7ee
Manoj <manojparasuram.sadhanala@gmail.com>
2022-10-20 22:11:37 +0530

    error faced in encrytping

--- sqlite/Program.cs
+++ sqlite/Program.cs
@@ -32,7 +32,7 @@ public class sqlite
                 Console.WriteLine(message);
             }
         }
-        dbConnection.ChangePassword("mypassword");
+        //dbConnection.ChangePassword("mypassword");
         closeConnection();
     }
 
